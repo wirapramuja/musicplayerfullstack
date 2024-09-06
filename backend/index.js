b417c8058e30013a74f1c5b74a62577f991be6b7
wirapramuja <wiraapramuja44@gmail.com>
2024-09-06 17:31:29 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -13,16 +13,35 @@ const port = process.env.PORT || 4000;
 connectDB()
 connectCloudinary()
 
+const allowedOrigins = [
+  'https://musicplayer-admin.vercel.app',
+  'https://musicplayer-frontend.vercel.app'
+];
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true // Jika menggunakan cookies atau sesi
+  })
+);
+app.use(cors());
 
 // initializing routes
 
-app.use("/api/song", songRouter)
-app.use("/api/album", albumRouter)
+app.use('/api/song', songRouter);
+app.use('/api/album', albumRouter);
 
-app.get('/',(req, res)=>res.send("API Working"))
+app.get('/', (req, res) => res.send('API Working'));
+
+app.options('*', cors());
 
 app.listen(port, () =>console.log(`Server started on ${port}`))
